f54a1f6cdad11bb8da5c35ab962146c8e3633088
M Subhan <125981621+subhan-2207e@users.noreply.github.com>
2024-06-13 12:23:12 +0500

    Add files via upload
    Typescript Word Counter

---
+++ main.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bold.cyanBright("\n \t Subhan Khan - Word counter \n"));
+console.log("=".repeat(60));
+
+let answer = await inquirer.prompt([
+    {
+        name: "sentence",  // changed to lower case 's'
+        type: "input",
+        message: "Enter a sentence :-",
+    },
+]);
+
+let Words = answer.sentence.trim().split(" ");
+console.log("=".repeat(60));
+console.log(chalk.bold("- sentence Word :-"));
+console.log(Words);
+console.log(`${chalk.bold.redBright(Words.length)}`);  // changed to 'length'
+console.log("=".repeat(60));  // corrected to match the upper line
+
+
+
+
